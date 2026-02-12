933942c2d8d9e7bb297776ad83c6fa19deef40c7
NikitaBorovikov <nikitaborovikov23@yandex.ru>
2026-02-12 20:51:28 +0300

    feature: implement golangci-lint plugin support

--- plugin/main.go
+++ plugin/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"loglinter/loglinter"
 
+	"golang.org/x/tools/go/analysis"
+)
+
+func New(conf any) ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{
+		loglinter.Analyzer,
+	}, nil
 }
